8d71faa088adb3574b432be2524f6f752fbf62ff
Higor Morais <higor.morais@escolar.ifrn.edu.br>
2023-05-04 16:04:53 -0300

    arquivos base para template de modelos de tarefas

---
+++ pilha.py
@@ -0,0 +1,38 @@
+# -*- coding:UTF-8 -*-
+from no import No
+
+class Pilha:
+    def __init__(self, capacidade=5):
+        self.__topo = None
+        self.__capacidade = capacidade
+        self.__qtdItens = 0
+
+
+    def is_empty(self) -> bool:
+        # implementação do método
+        pass
+
+
+    def push(self, valor) -> bool:
+        # implementação do método
+        pass
+
+
+    def pop(self) -> No:
+        # implementação do método
+        pass
+
+
+    def peek(self) -> No:
+        # implementação do método
+        pass
+
+
+    def list_items(self) -> str:
+        # implementação do método
+        pass
+    
+
+    def get_size(self):
+        # implementação do método
+        pass
